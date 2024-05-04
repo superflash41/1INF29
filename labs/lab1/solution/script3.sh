c521c82fd719388d9296d172bcaad17af5400c26
superflash41 <saymonnichorayo@gmail.com>
2024-05-04 14:38:08 -0400

    add lab3

--- labs/lab1/solution/script3.sh
+++ labs/lab1/solution/script3.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+# superflash41
 while IFS= read -r line; do
 	words=($line)
 	first="${words[0]}"
 	unset 'words[0]'
-	# words="${words[@]^}"  
 	words+=("$first")
 	new_ln=$(IFS=' '; echo "${words[@]^}")
 	echo $new_ln >> newdata
